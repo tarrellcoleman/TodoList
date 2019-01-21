048d1b6d1db71dd6b6a01894dbfb98269a6b2539
Tarrell Coleman <tarrell.coleman17@gmail.com>
2019-01-20 22:46:17 -0600

    Added pictures and new inputs for pictures-WIP

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,23 +8,36 @@
         id ="new-task"
         placeholder="Add Task"
       >
-      <button>Submit</button>
+      <input type="image" name="sumbit"
+            src="../content/submitButton.png"
+            border="0" alt="Submit" style="width: 20px;" />
       <br>
     </form>
     <ul>
       <li v-for="(task, index) in listOfTasks" :key="task.id">
         {{ task.task }}
-      <button v-on:click="deleted(index)">Delete</button>
-      <button v-on:click="task.edited=true">Edit</button>
+      <input v-on:click="deleted(index)"
+            type="image" name="delete"
+            src="../content/deleteButton.png"
+            border="0" alt="Delete" style="width: 20px;" />
+      <input v-on:click="task.edited=true"
+            type="image" name="edit"
+            src="../content/editButton.png"
+            border="0" alt="Edit" style="width: 20px;" />
       <br>
       <input v-if="task.edited === true"
           v-model= "editedTask"
           id ="edit-task"
           placeholder= "Edit Task">
-          <button v-if="task.edited === true"
-                  v-on:click="edited(index)">Submit</button>
-          <button v-if="task.edited === true"
-                  v-on:click="task.edited=false">Cancel</button>
+      <input v-if="task.edited === true"
+            v-on:click="edited(index)"
+            type="image" name="sumbitEdited"
+            src="../content/submitButton.png"
+            border="0" alt="Edited" style="width: 20px;" />
+      <input v-if="task.edited=false"
+            type="image" name="cancel"
+            src="../content/cancelButton.png"
+            border="0" alt="Cancel" style="width: 20px;" />
       </li>
     </ul>
     </div>
